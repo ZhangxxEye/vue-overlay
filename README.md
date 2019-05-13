193bad53244b8cdda9925a2b3a4fb6dd6b3e9df9
zhangxiaoxia <zhangxiaoxia@51talk.com>
2019-05-13 20:13:38 +0800

    蒙层

---
+++ README.md
@@ -0,0 +1,20 @@
+# vue组件开发模板
+webpack构建的vue组件开发模板
+## install
+NPM
+```
+npm install
+```
+## 启动
+```
+npm run dev
+```
+## 编译
+```
+npm run build-lib
+```
+## 开发环境调试
+docs/pages目录中的test.vue文件中引入组件，启动本地服务调试
+```
+npm run dev
+```
---
+++ src/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="vue-overlay"  @touchmove="move" @touchend="handleClick" :style="{zIndex: zIndex}">
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "vue-overlay",
+      props: {
+        zIndex: Number
+      },
+      methods: {
+        move (e) {
+          e.preventDefault();
+          e.stopPropagation();
+        },
+        handleClick () {
+          this.$emit('click');
+        }
+      }
+    }
+</script>
+
+<style scoped lang="less">
+.vue-overlay{
+    background: rgba(0, 0, 0, 0.6);
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1000;
+}
+</style>
